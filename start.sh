8e1afab194f70a18f620bd80ffb8e76c9e22867b
Timothy Pulliam <timothy.dpulliam@gmail.com>
2023-12-27 22:07:24 -0500

    gunicorn listen on non-reserved port 8080

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gunicorn --bind 0.0.0.0:80 --workers 3 --access-logfile - --error-logfile - main:app
+gunicorn --bind 0.0.0.0:8080 --workers 3 --access-logfile - --error-logfile - main:app
